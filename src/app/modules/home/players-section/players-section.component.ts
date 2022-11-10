48574ec23d5a9b31e48b920adde644c5e24c8a63
anajuliaalvarenga <ana.julia@b2mlportal.com.br>
2022-11-10 14:43:11 -0300

    feat: initial design of the players section change

--- src/app/modules/home/players-section/players-section.component.ts
+++ src/app/modules/home/players-section/players-section.component.ts
@@ -1,5 +1,6 @@
 import { Component, HostListener } from '@angular/core';
 import { playersList } from 'src/app/data/players';
+import { IPlayer } from 'src/app/interfaces/IPlayers';
 
 @Component({
   selector: 'app-players-section',
@@ -7,97 +8,29 @@ import { playersList } from 'src/app/data/players';
   styleUrls: ['./players-section.component.scss']
 })
 export class PlayersSectionComponent  {
-  playersCarousel = playersList.slice(0, 3);
-  leftCardIndex = 0;
-  rightCardIndex = 2;
+  playersCarousel = playersList.slice();
+  listWidth = this.playersCarousel.length * 200;
+  scrollX = 0;
+  selectedPlayer = this.playersCarousel[0];
 
-  mobileMode = false;
-  mobileCardIndex = 0;
-
-  constructor() {
-    this.onResize();
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize() {
-    this.mobileMode = window.innerWidth < 1024;
-    if(this.mobileMode) {
-      this.playersCarousel = [playersList[0]];
-      this.mobileCardIndex = 0;
-    } else {
-      this.playersCarousel = playersList.slice(0, 3);
-      this.leftCardIndex = 0;
-      this.rightCardIndex = 2;
+  onPreviousPlayer() {
+    let newScrollX = this.scrollX + Math.round(window.innerWidth / 4);
+    if(newScrollX > 0) {
+        newScrollX = 0;
     }
+    this.scrollX = newScrollX;
   }
 
   onNextPlayer() {
-    this.mobileMode ? this.nextPlayerMobile() : this.nextPlayerDesktop();
-  }
-
-  onPreviousPlayer() {
-    this.mobileMode ? this.previousPlayerMobile() : this.previousPlayerDesktop();
-  }
-
-  nextPlayerDesktop() {
-    if(this.rightCardIndex === (playersList.length - 1)) {
-      this.rightCardIndex = 0;
-      this.leftCardIndex++;
-      this.playersCarousel.shift();
-      this.playersCarousel.push(playersList[0]);
-    } else if (this.leftCardIndex === (playersList.length - 1)) {
-      this.leftCardIndex = 0;
-      this.rightCardIndex++
-      this.playersCarousel.shift();
-      this.playersCarousel.push(playersList[this.rightCardIndex]);
-    } else {
-      this.rightCardIndex++;
-      this.leftCardIndex++
-      this.playersCarousel.shift();
-      this.playersCarousel.push(playersList[this.rightCardIndex]);
+    let newScrollX = this.scrollX - Math.round(window.innerWidth / 4);
+    console.log({newScrollX});
+    if((window.innerWidth - this.listWidth) > newScrollX) {
+        newScrollX = (window.innerWidth - this.listWidth) - 60;
     }
+    this.scrollX = newScrollX;
   }
 
-  previousPlayerDesktop() {
-    if(this.leftCardIndex === 0) {
-      this.leftCardIndex = (playersList.length - 1);
-      this.rightCardIndex--;
-      this.playersCarousel.pop();
-      this.playersCarousel.unshift(playersList[playersList.length - 1]);
-    } else if (this.rightCardIndex === 0) {
-      this.leftCardIndex--;
-      this.rightCardIndex = (playersList.length - 1);
-      this.playersCarousel.pop();
-      this.playersCarousel.unshift(playersList[this.leftCardIndex]);
-    } else {
-      this.rightCardIndex--;
-      this.leftCardIndex--;
-      this.playersCarousel.pop();
-      this.playersCarousel.unshift(playersList[this.leftCardIndex]);
-    }
-  }
-
-  nextPlayerMobile() {
-    if(this.mobileCardIndex === (playersList.length - 1)) {
-      this.mobileCardIndex = 0;
-      this.playersCarousel.pop();
-      this.playersCarousel.push(playersList[0]);
-    } else {
-      this.mobileCardIndex++;
-      this.playersCarousel.pop();
-      this.playersCarousel.push(playersList[this.mobileCardIndex]);
-    }
-  }
-
-  previousPlayerMobile() {
-    if(this.mobileCardIndex === 0) {
-      this.mobileCardIndex = (playersList.length - 1);
-      this.playersCarousel.pop();
-      this.playersCarousel.push(playersList[playersList.length - 1]);
-    } else {
-      this.mobileCardIndex--;
-      this.playersCarousel.pop();
-      this.playersCarousel.push(playersList[this.mobileCardIndex]);
-    }
+  onSelectPlayer(player: IPlayer) {
+    this.selectedPlayer = player;
   }
 }
